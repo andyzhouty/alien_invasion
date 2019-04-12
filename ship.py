a553b49a53d98c2bc5398e74c1ca1987c45af84b
Andy Zhou <andyforever0108@outlook.com>
2019-04-12 20:47:44 +0800

    limiting the ship's range

--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship():
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
-        self.settings = ai_settings
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -25,10 +25,13 @@ class Ship():
 
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
