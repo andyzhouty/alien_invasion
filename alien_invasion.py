f0550c28dc428be7081f27a63541a8eaa94bdb9d
Andy Zhou <andyforever0108@outlook.com>
2019-04-18 13:11:16 +0800

    Firing Bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
 import pygame
-
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -16,12 +16,15 @@ def run_game():
 
     # Make s ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
