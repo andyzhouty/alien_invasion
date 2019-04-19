1678c5d1e80f1c1f3186a8aacfe27f20c8115e9f
Andy Zhou <andyforever0108@outlook.com>
2019-04-19 15:43:36 +0800

    Moving the aliens right.

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,6 +29,7 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
+        gf.update_aliens(aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
