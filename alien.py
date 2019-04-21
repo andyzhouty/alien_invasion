e04af8b5bbcf8e761bb88e693645823b69841bf6
Andy Zhou <andyforever0108@outlook.com>
2019-04-21 17:06:34 +0800

    Dropping the fleet and changing direction.

--- alien.py
+++ alien.py
@@ -39,4 +39,3 @@ class Alien(Sprite):
         self.x += (self.ai_settings.alien_speed_factor *
                    self.ai_settings.fleet_direction)
         self.rect.x = self.x
-
--- alien_invasion.py
+++ alien_invasion.py
@@ -29,7 +29,7 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_aliens(aliens)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
