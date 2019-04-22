c5af739b590f50cc023421e54ebf95f919b9cf24
Andy Zhou <andyforever0108@outlook.com>
2019-04-22 19:25:10 +0800

    Detecting Bullet Collisions

--- game_functions.py
+++ game_functions.py
@@ -62,7 +62,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
     # Update bullet positions.
     bullets.update()
@@ -71,6 +71,10 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+    # Check for any bullets that have hit aliens.
+    # If so, git rid of the bullet and the alien.
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 def get_number_aliens_x(ai_settings, alien_width):
