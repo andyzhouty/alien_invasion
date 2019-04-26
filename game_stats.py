6cb64ac891ef779bdfdc352c4e0038480b4ff60b
Andy Zhou <andyforever0108@outlook.com>
2019-04-26 19:42:46 +0800

    Game over!

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class Gamestats():
         self.ai_settings = ai_settings
         self.reset_stats()
 
+        # Start Alien Incasion in an active state.
+        self.game_active = True
+
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
